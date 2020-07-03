5f9cbcf62f3482221788c2b294bc9e6b5debe90a
Alvin <alvinzcoxiv@gmail.com>
2020-07-02 22:49:45 -0400

    Displays current weather for searched city

--- script.js
+++ script.js
@@ -11,6 +11,44 @@ $(document).ready(function() {
             method: "GET"
         }).then(function(response) {
             console.log(response)
+
+            var cityName = response.name
+            var cityDateDT = response.dt
+            var cityLat = response.coord.lat
+            var cityLon = response.coord.lon
+
+            function convertTime(unixTime) {
+                var a = new Date(cityDateDT * 1000)
+                var year = a.getFullYear()
+                var month = a.getMonth() + 1
+                var date = a.getDate()
+                var time = month + "/" + date + "/" + year
+                return time
+            }
+
+            var cityDateSTND = convertTime(cityDateDT)
+
+            var cityNameh1One = $("<h1>").text(cityName)
+            $("#cityid").append(cityNameh1One)
+
+            var cityDateh2 = $("<h2>").text(cityDateSTND)
+            $("#citydate").append(cityDateh2)
+
+            $("#temperature").text("Temperature: " + response.main.temp)
+            $("#humidity").text("Humidity: " + response.main.humidity)
+            $("#windspeed").text("Windspeed: " + response.wind.speed)
+
+            var queryULRuv = "http://api.openweathermap.org/data/2.5/uvi?appid=454601f0e3a2d5cad022194c58ec0c3a&lat=" + cityLat + "&lon=" + cityLon
+
+            $.ajax({
+                url: queryULRuv,
+                method: "GET"
+            }).then(function(uvResponse) {
+                $("#uvindex").text("UV Index: " + uvResponse.value)
+            })
+
+            
+
         })
     }
 
@@ -22,7 +60,7 @@ $(document).ready(function() {
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response)
+            // console.log(response)
         })
     }
 

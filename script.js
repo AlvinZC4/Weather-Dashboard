5e63b5893415c7d8f04b745e2dd1fc16600766c8
Alvin <alvinzcoxiv@gmail.com>
2020-07-03 00:19:10 -0400

    new searches now clear display of previous searches before displaying current search. 5 day forecast now shown for searched cities

--- script.js
+++ script.js
@@ -1,45 +1,90 @@
 $(document).ready(function() {
-    var searchedCity = $("#searchedcity");
+    // Function to clear display contents before repopulating the display with results from a new search
+    function clearDisplayCurrent() {
+        $("#cityname").text("")
+        $("#citydate").text("")
+        $("#temperature").text("")
+        $("#humidity").text("")
+        $("#windspeed").text("")
+    }
 
+    function clearDisplayForecast() {
+        $("#dayonedate").text("")
 
+        $("#dayonetemp").text("")
+        $("#dayonehumidity").text("")
+        $("#daytwodate").text("")
+
+        $("#daytwotemp").text("")
+        $("#daytwohumidity").text("")
+        $("#daythreedate").text("")
+
+        $("#daythreetemp").text("")
+        $("#daythreehumidity").text("")
+        $("#dayfourdate").text("")
+
+        $("#dayfourtemp").text("")
+        $("#dayfourhumidity").text("")
+        $("#dayfivedate").text("")
+
+        $("#dayfivetemp").text("")
+        $("#dayfivehumidity").text("")
+    }
+    // Function to convert unix time to a current date
+    function convertTime(unixTime) {
+        var a = new Date(unixTime * 1000)
+        var year = a.getFullYear()
+        var month = a.getMonth() + 1
+        var date = a.getDate()
+        var time = month + "/" + date + "/" + year
+        return time
+    }
+
+    function convertTemp(tempKev) {
+       var tempFlng = (tempKev - 273.15) * 1.80 + 32
+       var tempF = tempFlng.toFixed(2)
+       return tempF
+    }
+
+    // Function to display the current weather for the searched city
     function displayCityWeather(selectedCity) {
+        clearDisplayCurrent()
+
+        // Variables for the city that is being serched and the URL to query the weather API
         var city = selectedCity
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=454601f0e3a2d5cad022194c58ec0c3a"
 
+        // Query the weather API for current weather data on the searched city
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response)
+            // console.log(response)
 
+            // Declare variables that contain data from the obejct returned form the ajax call to display data or use to generate more data
             var cityName = response.name
             var cityDateDT = response.dt
             var cityLat = response.coord.lat
             var cityLon = response.coord.lon
-
-            function convertTime(unixTime) {
-                var a = new Date(cityDateDT * 1000)
-                var year = a.getFullYear()
-                var month = a.getMonth() + 1
-                var date = a.getDate()
-                var time = month + "/" + date + "/" + year
-                return time
-            }
-
+            var cityTempKev = response.main.temp
+            
+            // Execute the converTime function from line 12
             var cityDateSTND = convertTime(cityDateDT)
 
-            var cityNameh1One = $("<h1>").text(cityName)
-            $("#cityid").append(cityNameh1One)
+            var cityTempF = convertTemp(cityTempKev)
 
-            var cityDateh2 = $("<h2>").text(cityDateSTND)
-            $("#citydate").append(cityDateh2)
+            // Display elements of retured obejct on html
+            $("#cityname").text(cityName)
+            $("#citydate").text(cityDateSTND)
 
-            $("#temperature").text("Temperature: " + response.main.temp)
+            $("#temperature").text("Temperature: " + cityTempF)
             $("#humidity").text("Humidity: " + response.main.humidity)
             $("#windspeed").text("Windspeed: " + response.wind.speed)
 
+            // A different URL is used to obtain the UV Index of the searched city.  Use Latitude and Longitude obtained form the first ajax call
             var queryULRuv = "http://api.openweathermap.org/data/2.5/uvi?appid=454601f0e3a2d5cad022194c58ec0c3a&lat=" + cityLat + "&lon=" + cityLon
 
+            // Query weather API to get UV Index of searched city and display the (desired) retured data on the html
             $.ajax({
                 url: queryULRuv,
                 method: "GET"
@@ -53,6 +98,8 @@ $(document).ready(function() {
     }
 
     function displayCityForecast(selectedCity) {
+        clearDisplayForecast()
+
         var city = selectedCity
         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=454601f0e3a2d5cad022194c58ec0c3a"
 
@@ -60,7 +107,50 @@ $(document).ready(function() {
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            // console.log(response)
+            console.log(response)
+
+            var dayOneDateDT = response.list[3].dt
+            var dayTwoDateDT = response.list[11].dt
+            var dayThreeDateDT = response.list[19].dt
+            var dayFourDateDT = response.list[27].dt
+            var dayFiveDateDT = response.list[35].dt
+            var dayOneTempKev = response.list[3].main.temp
+            var dayTwoTempKev = response.list[11].main.temp
+            var dayThreeTempKev = response.list[19].main.temp
+            var dayFourTempKev = response.list[27].main.temp
+            var dayFiveTempKev = response.list[35].main.temp
+
+            var dayOneDateSTND = convertTime(dayOneDateDT)
+            var dayTwoDateSTND = convertTime(dayTwoDateDT)
+            var dayThreeDateSTND = convertTime(dayThreeDateDT)
+            var dayFourDateSTND  = convertTime(dayFourDateDT)
+            var dayFiveDateSTND = convertTime(dayFiveDateDT)
+            var dayOneTempF = convertTemp(dayOneTempKev)
+            var dayTwoTempF = convertTemp(dayTwoTempKev)
+            var dayThreeTempF = convertTemp(dayThreeTempKev)
+            var dayFourTempF = convertTemp(dayFourTempKev)
+            var dayFiveTempF = convertTemp(dayFiveTempKev)
+
+            $("#dayonedate").text(dayOneDateSTND)
+
+            $("#dayonetemp").text("Temp: " + dayOneTempF)
+            $("#dayonehumidity").text("Humidity :" + response.list[3].main.humidity)
+            $("#daytwodate").text(dayTwoDateSTND)
+
+            $("#daytwotemp").text("Temp: " + dayTwoTempF)
+            $("#daytwohumidity").text("Humidity :" + response.list[11].main.humidity)
+            $("#daythreedate").text(dayThreeDateSTND)
+
+            $("#daythreetemp").text("Temp: " + dayThreeTempF)
+            $("#daythreehumidity").text("Humidity :" + response.list[19].main.humidity)
+            $("#dayfourdate").text(dayFourDateSTND)
+
+            $("#dayfourtemp").text("Temp: " + dayFourTempF)
+            $("#dayfourhumidity").text("Humidity :" + response.list[27].main.humidity)
+            $("#dayfivedate").text(dayFiveDateSTND)
+
+            $("#dayfivetemp").text("Temp: " + dayFiveTempF)
+            $("#dayfivehumidity").text("Humidity :" + response.list[35].main.humidity)
         })
     }
 
